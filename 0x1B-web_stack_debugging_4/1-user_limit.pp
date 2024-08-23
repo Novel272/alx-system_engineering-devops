331501df1f744b57ae0f7235309135dfbd242a75
Novel272 <mohammedalmustafak@gmail.com>
2024-08-23 01:14:19 +0000

    fg

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,6 @@
-hanges the OS configuration so that it is possible to login with the holberton user and open a file without any error message
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+# login with holberton user an opn a file without any error message.
+file { 'loginFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#File erased'
+}
